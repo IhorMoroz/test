223409e12949c8c339818432ae466c6b1f531e24
IhorMoroz <work.ihor.m@gmail.com>
2016-08-07 15:44:44 +0300

    add comments

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <?php
 
+/**
+ * [showName description]
+ * @param  string $name [description]
+ * @return [type]       [description]
+ */
 function showName($name = 'Anonim')
 {
     return "Hello - " . $name;
 }
 
+/**
+ * [showSurname description]
+ * @param  string $surname [description]
+ * @return [type]          [description]
+ */
 function showSurname($surname = '')
 {
     return  ' ' . $surname;
 }
 
+/**
+ * [showFullName]
+ * @param  [string] $name    [user name]
+ * @param  [string] $surname [user surname]
+ * @return [string]          [user full name]
+ */
 function showFullName($name,$surname)
 {
     return showName($name) . showSurname($surname);
